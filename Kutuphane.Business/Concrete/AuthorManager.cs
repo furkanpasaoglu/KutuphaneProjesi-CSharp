a66f5679d84eac19406ad4a774da243b6d055b70
Furkan Paşaoğlu <16624085+furkanpasaoglu@users.noreply.github.com>
2021-02-25 14:31:48 +0300

    Clean Code

--- Kutuphane.Business/Concrete/AuthorManager.cs
+++ Kutuphane.Business/Concrete/AuthorManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Kutuphane.Business.Abstract;
 using Kutuphane.Business.Constant;
+using Kutuphane.Core.Kutuphane.Utilities.Business;
 using Kutuphane.Core.Kutuphane.Utilities.Results;
 using Kutuphane.DataAccess.Abstract;
 using Kutuphane.Entities.Concrete;
@@ -20,50 +21,82 @@ namespace Kutuphane.Business.Concrete
 
         public IDataResult<List<Author>> GetList(string p = "")
         {
-            if (!String.IsNullOrEmpty(p))
+            var result = BusinessRules.Run2(CheckByQueryBlank(p));
+            if (result!=null)
             {
-                return new SuccessDataResult<List<Author>>(_authorDal.GetList().Where(x => x.Name.Contains(p)).ToList(), Messages.YazarListele);
+                return new SuccessDataResult<List<Author>>(_authorDal.GetList().ToList(), Messages.YazarListele);
             }
-            return new SuccessDataResult<List<Author>>(_authorDal.GetList().ToList(),Messages.YazarListele);
+            return new SuccessDataResult<List<Author>>(_authorDal.GetList().Where(x => x.Name.Contains(p)).ToList(), Messages.YazarListele);
         }
 
         public IDataResult<Author> GetById(int id)
         {
-            if (id > 0)
-            {
-                return new SuccessDataResult<Author>(_authorDal.GetById(p => p.Id == id));
-            }
-            return new ErrorDataResult<Author>(Messages.Hata);
+            var result = BusinessRules.Run2(CheckByIdBlank(id));
+            if (result!=null)
+                return new ErrorDataResult<Author>(Messages.Hata);
+
+            return new SuccessDataResult<Author>(_authorDal.GetById(p => p.Id == id));
+
         }
 
         public IResult Add(Author author)
         {
-            if (author != null)
-            {
-                _authorDal.Add(author);
-                return new SuccessResult(Messages.YazarEkle);
-            }
-            return new ErrorResult(Messages.Hata);
+            var result = BusinessRules.Run(CheckEntityBlank(author));
+            if (result != null)
+                return new ErrorResult(Messages.Hata);
+
+            _authorDal.Add(author);
+            return new SuccessResult(Messages.YazarEkle);
         }
 
         public IResult Update(Author author)
         {
-            if (author != null)
-            {
-                _authorDal.Update(author);
-                return new SuccessResult(Messages.YazarGüncelle);
-            }
-            return new ErrorResult(Messages.Hata);
+            var result = BusinessRules.Run(CheckEntityBlank(author));
+            if (result != null)
+                return new ErrorResult(Messages.Hata);
+
+            _authorDal.Update(author);
+            return new SuccessResult(Messages.YazarGüncelle);
         }
 
         public IResult Delete(Author author)
         {
-            if (author != null)
+            var result = BusinessRules.Run(CheckEntityBlank(author));
+            if (result != null)
+                return new ErrorResult(Messages.Hata);
+
+            _authorDal.Delete(author);
+            return new SuccessResult(Messages.YazarSil);
+        }
+
+        private IResult CheckEntityBlank(Author author)
+        {
+            if (author == null)
             {
-                _authorDal.Delete(author);
-                return new SuccessResult(Messages.YazarSil);
+                return new ErrorResult(Messages.Hata);
             }
-            return new ErrorResult(Messages.Hata);
+
+            return new SuccessResult();
+        }
+
+        private IDataResult<object> CheckByIdBlank(int id)
+        {
+            if (id > 0)
+            {
+                return new SuccessDataResult<object>();
+            }
+
+            return new ErrorDataResult<object>(Messages.Hata);
+        }
+
+        private IDataResult<object> CheckByQueryBlank(string p)
+        {
+            if (!String.IsNullOrEmpty(p))
+            {
+                return new SuccessDataResult<object>();
+            }
+
+            return new ErrorDataResult<object>();
         }
     }
 }
---
+++ Kutuphane.Core/Kutuphane.Utilities/Business/BusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Kutuphane.Core.Kutuphane.Utilities.Results;
+
+namespace Kutuphane.Core.Kutuphane.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                    return logic;
+            }
+
+            return null;
+        }
+
+        public static IDataResult<object> Run2(params IDataResult<object>[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                    return logic;
+            }
+            return null;
+
+        }
+    }
+}
